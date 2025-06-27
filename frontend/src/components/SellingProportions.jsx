e8c4597b291ebad4bd9944bcf7b7ae719d543cf2
saadakhtr29 <saadakhtr51@gmail.com>
2025-06-28 01:59:01 +0530

    completed the selling proportion frame, with some text modifications in the fancy frame and featured products sections as well. Also fixed the file import typo in the main App.jsx

--- frontend/src/components/SellingProportions.jsx
+++ frontend/src/components/SellingProportions.jsx
@@ -8,7 +8,19 @@ export default function SellingProportions() {
       <h1>Have a Look at Our Unique Selling Proportions</h1>
       <button className="read-more-button">Read More</button>
       </div>
-      
+      <div className='selling-proportions-texts'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+       <div className='selling-proportions-subtext'>
+         <div className='selling-proportions-text-items'>
+          <h2>99%</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit.</p>
+        </div>
+        <div className='selling-proportions-text-items'>
+          <h2>100%</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit.</p>
+        </div>
+       </div>
+      </div>
      </div>
     </div>
   );
