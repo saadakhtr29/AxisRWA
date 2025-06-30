32b873f916781832f3fab089437acdcd42951043
saadakhtr29 <saadakhtr51@gmail.com>
2025-06-30 23:33:17 +0530

    added login page to the app.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignupPage from "./pages/Signup";
+import LoginPage from "./pages/Login";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
+import Authgif from "../assets/a4cf01a2edfaf1b62c83b31ab78361e5.gif"
+import "../styles/loginPage.css";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const handleLogin = async () => {
+    try {
+      const userCredential = await login(email, password);
+      const token = await userCredential.user.getIdToken();
+      localStorage.setItem("authToken", token);
+      alert("Welcome back!");
+      navigate("/dashboard");
+    } catch (e) {
+      alert("Login failed: " + e.message);
+    }
+  };
+
+  return (
+    <div className="login-page-wrapper">
+      <div className="login-container">
+        <img
+          className="login-gif"
+          src={Authgif}
+          alt="Login-gif"
+        />
+        <div className="login-contents">
+          <h1 className="login-header">Welcome back!</h1>
+          <p className="login-text">
+            Don't have an account?{" "}
+            <a className="signup-link" href="/signup">
+              Sign up
+            </a>
+          </p>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="login-inputs">
+            <button className="login-btn" onClick={handleLogin}>
+              Login
+            </button>
+            {/* <button className="google-signin-btn">Sign in with Google</button> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
